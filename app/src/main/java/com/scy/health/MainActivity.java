58ec0b5f5282653901251e7221b8ffff8c75b89c
NepoleonAsus <835410808@qq.com>
2018-04-03 18:47:37 +0800

    唤醒和识别

--- app/src/main/java/com/scy/health/MainActivity.java
+++ app/src/main/java/com/scy/health/MainActivity.java
@@ -2,12 +2,126 @@ package com.scy.health;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.baidu.speech.EventListener;
+import com.baidu.speech.EventManager;
+import com.baidu.speech.EventManagerFactory;
+import com.baidu.speech.asr.SpeechConstant;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MainActivity extends AppCompatActivity implements EventListener {
+    private EventManager wakeup;
+    private EventManager asr;
+    private boolean logTime = false;
+    private boolean enableOffline = true; // 测试离线命令词，需要改成true
+    private void start_wk() {
+        Map<String, Object> params = new TreeMap<String, Object>();
+        params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, false);
+        params.put(SpeechConstant.WP_WORDS_FILE, "assets:///WakeUp.bin");
+        // "assets:///WakeUp.bin" 表示WakeUp.bin文件定义在assets目录下
+        String json = null; // 这里可以替换成你需要测试的json
+        json = new JSONObject(params).toString();
+        wakeup.send(SpeechConstant.WAKEUP_START, json, null, 0, 0);
+    }
+    private void start_asr() {
+        Map<String, Object> params = new LinkedHashMap<String, Object>();
+        String event = SpeechConstant.ASR_START; // 替换成测试的event
+
+        if (enableOffline) {
+            params.put(SpeechConstant.DECODER, 2);
+        }
+        params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, false);
+        // params.put(SpeechConstant.NLU, "enable");
+        // params.put(SpeechConstant.VAD_ENDPOINT_TIMEOUT, 0); // 长语音
+        // params.put(SpeechConstant.IN_FILE, "res:///com/baidu/android/voicedemo/16k_test.pcm");
+        // params.put(SpeechConstant.VAD, SpeechConstant.VAD_DNN);
+        // params.put(SpeechConstant.PROP ,20000);
+        // params.put(SpeechConstant.PID, 1537); // 中文输入法模型，有逗号
+        // 请先使用如‘在线识别’界面测试和生成识别参数。 params同ActivityRecog类中myRecognizer.start(params);
+
+        // 复制此段可以自动检测错误
+        String json = null; // 可以替换成自己的json
+        json = new JSONObject(params).toString(); // 这里可以替换成你需要测试的json
+        asr.send(event, json, null, 0, 0);
+        printLog("输入参数：" + json);
+    }
+    private void loadOfflineEngine() {
+        Map<String, Object> params = new LinkedHashMap<String, Object>();
+        params.put(SpeechConstant.DECODER, 2);
+        params.put(SpeechConstant.ASR_OFFLINE_ENGINE_GRAMMER_FILE_PATH, "assets://baidu_speech_grammar.bsg");
+        asr.send(SpeechConstant.ASR_KWS_LOAD_ENGINE, new JSONObject(params).toString(), null, 0, 0);
+    }
+
+    private TextView res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wakeup = EventManagerFactory.create(this, "wp");
+        wakeup.registerListener(this); //  EventListener 中 onEvent方法
+        asr = EventManagerFactory.create(this, "asr");
+        asr.registerListener(this); //  EventListener 中 onEvent方法
+        start_wk();
+        if (enableOffline) {
+            loadOfflineEngine(); // 测试离线命令词请开启, 测试 ASR_OFFLINE_ENGINE_GRAMMER_FILE_PATH 参数时开启
+        }
+
+        res = (TextView)findViewById(R.id.res);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        wakeup.send(SpeechConstant.WAKEUP_STOP, "{}", null, 0, 0);
+        asr.send(SpeechConstant.ASR_STOP, null, null, 0, 0); //
+        if (enableOffline) {
+            asr.send(SpeechConstant.ASR_KWS_UNLOAD_ENGINE, null, null, 0, 0); //
+        }
+    }
+
+    //EventListener  回调方法
+    @Override
+    public void onEvent(String name, String params, byte[] data, int offset, int length) {
+        String logTxt = "name: " + name;
+        if (params != null && !params.isEmpty()) {
+            logTxt += " ;params :" + params;
+        }
+        if (name.equals(SpeechConstant.CALLBACK_EVENT_WAKEUP_SUCCESS)){     //唤醒事件
+            wakeup.send(SpeechConstant.WAKEUP_STOP, "{}", null, 0, 0);      //关闭wk
+            start_asr();
+        }
+        else if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_PARTIAL)) {
+            if (params.contains("\"results_recognition\"")&&params.contains("final_result")) {
+                try {
+                    JSONObject jsStr = new JSONObject(params);
+                    String sres = jsStr.getString("best_result");
+                    res.setText(sres);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            asr.send(SpeechConstant.ASR_STOP, null, null, 0, 0);        //关闭asr，移交给wk
+            start_wk();
+        }
+        else if (data != null) {
+            logTxt += " ;data length=" + data.length;
+        }
+        printLog(logTxt);
+    }
+
+    private void printLog(String text) {
+        if (logTime) {
+            text += "  ;time=" + System.currentTimeMillis();
+        }
+        text += "\n";
+        Log.i(getClass().getName(), text);
     }
 }
