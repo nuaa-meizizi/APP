c4bb22d87a8391c818c36754eb0bfd178cb91b34
NepoleonAsus <835410808@qq.com>
2018-07-21 22:58:21 +0800

    注册

---
+++ app/src/main/java/com/scy/health/AsyncTasks/SignUpTask.java
@@ -0,0 +1,113 @@
+package com.scy.health.AsyncTasks;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.scy.health.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class SignUpTask extends AsyncTask<Void, Void, JSONObject> {
+    private final String mEmail;
+    private final String mPassword;
+    String loginUrl = "http://app.logicjake.xyz:8080/health/user/signup";
+    private Context context;
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private Button mEmailSignInButton;
+    private SweetAlertDialog dialog;
+    private static final String TAG = "SignUpTask";
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    public SignUpTask(Context context, SweetAlertDialog dialog,Button mEmailSignInButton, EditText mEmailView, EditText mPasswordView, String email, String password) {
+        this.context = context;
+        this.mEmailView = mEmailView;
+        this.dialog = dialog;
+        this.mEmailSignInButton = mEmailSignInButton;
+        this.mPasswordView = mPasswordView;;
+        mEmail = email;
+        mPassword = password;
+        sharedPreferences = context.getSharedPreferences("setting", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    @Override
+    protected JSONObject doInBackground(Void... params) {
+        try {
+            String param = "name="+mEmail+"&password="+mPassword;
+            StringBuilder sb = new StringBuilder();
+            InputStream is = null;
+            BufferedReader br = null;
+            URL uri = new URL(loginUrl);
+            HttpURLConnection connection = (HttpURLConnection) uri.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setReadTimeout(5000);
+            connection.setConnectTimeout(10000);
+            connection.setRequestProperty("accept", "*/*");
+            //发送参数
+            connection.setDoOutput(true);
+            PrintWriter out = new PrintWriter(connection.getOutputStream());
+            out.print(param);
+            out.flush();
+            //接收结果
+            is = connection.getInputStream();
+            br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line;
+            //缓冲逐行读取
+            while ( ( line = br.readLine() ) != null ) {
+                sb.append(line);
+            }
+            Log.d(TAG, "doInBackground() returned: " + sb.toString());
+            JSONObject res = new JSONObject(sb.toString());
+            return  res;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(final JSONObject res) {
+        if (res != null) {
+            try {
+                if (res.getInt("status") == 0) {
+                    editor.putString("name", mEmail);
+                    editor.putString("password", mPassword);
+                    editor.putString("token", res.getJSONObject("data").getString("token"));
+                    editor.commit();
+                    ((Activity) context).finish();
+                }
+                else if (res.getInt("status") == 130000){
+                    mEmailView.setError(context.getString(R.string.error_user_existence));
+                    mEmailView.requestFocus();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            mEmailSignInButton.setClickable(true);
+            dialog.cancel();
+        }
+    }
+
+    @Override
+    protected void onCancelled() {
+        dialog.cancel();
+        mEmailSignInButton.setClickable(true);
+    }
+}
--- app/src/main/java/com/scy/health/activities/LoginActivity.java
+++ app/src/main/java/com/scy/health/activities/LoginActivity.java
@@ -2,9 +2,16 @@ package com.scy.health.activities;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,6 +20,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.scy.health.AsyncTasks.SignUpTask;
 import com.scy.health.AsyncTasks.UserLoginTask;
 import com.scy.health.R;
 
@@ -29,6 +37,8 @@ public class LoginActivity extends AppCompatActivity {
     private Button mEmailSignInButton;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private TextView changeFunction;
+    final SpannableStringBuilder style = new SpannableStringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +46,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         mEmailView = (EditText) findViewById(R.id.email);
         mPasswordView = (EditText) findViewById(R.id.password);
+        changeFunction = (TextView)findViewById(R.id.toRegister);
         sharedPreferences = getSharedPreferences("setting", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
 
@@ -43,7 +54,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
+                    attemptLoginOrSignup();
                     return true;
                 }
                 return false;
@@ -54,12 +65,58 @@ public class LoginActivity extends AppCompatActivity {
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                attemptLoginOrSignup();
             }
         });
+
+        toLoginAction();
     }
 
-    private void attemptLogin() {
+    private void toRegisterAction(){
+        setTitle("注册");
+        mPasswordView.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);              //明文
+        mEmailSignInButton.setText("注册");
+        style.clear();
+        style.append("返回登陆");
+        //设置部分文字点击事件
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                toLoginAction();
+            }
+        };
+        style.setSpan(clickableSpan, 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        changeFunction.setText(style);
+        //设置部分文字颜色
+        ForegroundColorSpan foregroundColorSpan = new ForegroundColorSpan(Color.BLUE);
+        style.setSpan(foregroundColorSpan, 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        changeFunction.setMovementMethod(LinkMovementMethod.getInstance());
+        changeFunction.setText(style);
+    }
+
+    private void toLoginAction(){
+        setTitle("登陆");
+        mPasswordView.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);              //明文
+        mEmailSignInButton.setText("登陆");
+        style.clear();
+        style.append("现在注册");
+        //设置部分文字点击事件
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                toRegisterAction();
+            }
+        };
+        style.setSpan(clickableSpan, 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        changeFunction.setText(style);
+        //设置部分文字颜色
+        ForegroundColorSpan foregroundColorSpan = new ForegroundColorSpan(Color.BLUE);
+        style.setSpan(foregroundColorSpan, 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        changeFunction.setMovementMethod(LinkMovementMethod.getInstance());
+        changeFunction.setText(style);
+    }
+
+    private void attemptLoginOrSignup() {
         dialog = new SweetAlertDialog(LoginActivity.this,SweetAlertDialog.PROGRESS_TYPE);
         dialog.setCancelable(false);
         dialog.show();
@@ -74,11 +131,13 @@ public class LoginActivity extends AppCompatActivity {
         View focusView = null;
 
         if (TextUtils.isEmpty(email)) {
+            dialog.cancel();
             mEmailView.setError(getString(R.string.error_field_required));
             focusView = mEmailView;
             cancel = true;
         }
         else if (TextUtils.isEmpty(password)) {
+            dialog.cancel();
             mPasswordView.setError(getString(R.string.error_field_required));
             focusView = mPasswordView;
             cancel = true;
@@ -95,6 +154,9 @@ public class LoginActivity extends AppCompatActivity {
             if (mEmailSignInButton.getText().toString().equals("登陆")){
                 mAuthTask = new UserLoginTask(this,dialog,mEmailSignInButton,mEmailView,mPasswordView,email, password);
                 mAuthTask.execute((Void) null);
+            }
+            else if (mEmailSignInButton.getText().toString().equals("注册")) {
+                new SignUpTask(this, dialog, mEmailSignInButton, mEmailView, mPasswordView, email, password).execute((Void) null);
             }
         }
     }
